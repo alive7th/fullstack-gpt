850e24a9f6a2ae078adef7c8655485dc047c76f5
alive7th <alive7th2@gmail.com>
2023-12-25 01:56:27 +0900

    #7.1 Magic

--- src/Chapters/home.py
+++ src/Chapters/home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
+
 
 st.title("Hello World!")
 
@@ -7,3 +9,22 @@ st.subheader("Welcome to Streamlit!")
 st.markdown("""
     #### I love it!!
 """)
+
+st.write("Hellow Im Jungwon")
+
+# st.write([1,2,3,4,5])
+a = [1,2,3,4,5]
+a
+
+# st.write({"name": "jwkim"})
+b = {"name": "jwkim"}
+
+b
+
+# st.write(PromptTemplate)
+PromptTemplate
+
+p = PromptTemplate.from_template("sssss")
+p
+# st.write(p)
+st.selectbox("선택하세요", ["GPT-3", "GPT-4"])
