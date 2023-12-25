0cfe75d93c1cf1753ce41236d301cfe8a2f0dc37
alive7th <alive7th2@gmail.com>
2023-12-26 01:55:31 +0900

    # 7.6 Uploading Documents

--- src/Chapters/pages/01_DocumentGPT.py
+++ src/Chapters/pages/01_DocumentGPT.py
@@ -1,34 +1,69 @@
 import streamlit as st
 import time
-
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📓"
 )
 
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader("./files/chapter_one.docx")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir 
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriver = vectorstore.as_retriever()
+    return retriver
+
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Welcome!
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-        if save:
-            st.session_state["messages"].append({"message": message, "role": role})
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
 
-# st.write(st.session_state["messages"])
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], False)
+file = st.file_uploader("Upload a .txt .pdf .docx file", type=["pdf","txt","docx"])
 
+if file:
+    retriever = embed_file(file)
+    retriever.invoke("개울가") 
 
-message = st.chat_input("AI에게 메시지를 보내세요!")
+# for doc in list of docs | prompt | llm
 
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
+# for response in list of llms response | put them all together
+# map_doc_prompt = ChatPromptTemplate.from_messages([
+#     (
+#         "system",
+#         """
+#             Use the following portion of a long document to see if any of the text is relevant to
+#             answer the question. Return any relevant text verbatim.
+#             ------
+#             {context}
+#         """
+#     ),
+#     (
+#         "human", "{question}"
+#     )
+# ])
